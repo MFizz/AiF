0b83974d5575774283948cea35a48254ba0f85a5
mitch <mitchfizz@gmail.com>
2015-01-10 18:41:43 +0100

    fixed missing parenthesis in utilityfnc

--- MultiAgentAdventures/Booker.py
+++ MultiAgentAdventures/Booker.py
@@ -16,13 +16,14 @@ class Booker:
         self.agents = agents
         self.adventures = adventures
 
-    print("Best adventures per adventurer")
     def run(self):
         """ Starts and controls the game process
 
         TODO: return evaluable data of the outcome of the game
         """
+        print("Best adventures per adventurer")
         for a in self.agents:
             print("Agent ID {}: {}".format(id(a), [(x, y, id(z)) for (x, y, z) in a.calcTopAdv(self.adventures)]))
+
 
 
