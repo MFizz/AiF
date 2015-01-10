45ebffbb5a3d02a8cad1a13ef1d4e19b7001408c
mitch <mitchfizz@gmail.com>
2015-01-10 14:46:50 +0100

    utilityfunction ausgelaget

--- MultiAgentAdventures/Agent.py
+++ MultiAgentAdventures/Agent.py
@@ -11,17 +11,18 @@ class Agent(object):
     def calcTopAdv(self, advetures):
         advValues = []
         for adv in advetures:
-            cost = self.costs.get(adv)
-            skills = 0
-            for skill, value in self.skillList:
-                if skill in adv.skillMap:
-                    skills += min(value, adv.skillMap.get(skill))
-            skillRatio = skills / sum(adv.skillMap.values())
-            cost = skillRatio * adv.reward
-            advValues.append((cost, adv))
+            advValues.append((self.utilityFunc(adv), adv))
         return sorted(advValues, key=lambda x: x[0], reverse=True)[0:4]
 
-
+    def utilityFunc(self, adventure):
+        cost = self.costs.get(adventure)
+        skills = 0
+        for skill, value in self.skillList:
+            if skill in adventure.skillMap:
+                skills += min(value, adventure.skillMap.get(skill))
+        skillRatio = skills / sum(adventure.skillMap.values())
+        cost += skillRatio * adventure.reward
+        return cost
 
 def _calcCostsAdv(self, adventures):
     costs = dict({})
@@ -32,7 +33,7 @@ def _calcCostsAdv(self, adventures):
 def createAgentList(t, advList):
     agentList = []
     for i in range(t):
-        skillList = [(x,random.randint(1,10)) for x in random.sample(list(Skill.Skill),2)]
+        skillList = [(x,random.randint(1, 10)) for x in random.sample(list(Skill.Skill),2)]
         agentList.append(Agent(skillList, advList))
     return agentList
 
