bd637daefd4d1eeb7663017a44a3c97bea1dd5bf
mitch <mitchfizz@gmail.com>
2015-02-07 20:10:22 +0100

    added gui for agents

--- MultiAgentAdventures/Booker.py
+++ MultiAgentAdventures/Booker.py
@@ -108,6 +108,10 @@ class Booker:
                 else:
                     adv.clean()
 
+            for agent in self.agents:
+                if len(agent.rewards) != i+2:
+                    agent.rewards.append(0)
+
             if rL:
                 roundsLeft -= 1
                 print('Completed adventures {}'.format(self.completedAdventures))
