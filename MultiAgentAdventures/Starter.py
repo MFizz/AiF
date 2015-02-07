b5ac82b9b45086e75289f31da9d84f32bc41188f
Mihail <mihailbogojeski@gmail.com>
2015-02-07 14:20:22 +0100

    greedy und upper bound

--- MultiAgentAdventures/Starter.py
+++ MultiAgentAdventures/Starter.py
@@ -15,7 +15,8 @@ numAgents = 15
 if __name__ == '__main__':
 
     bookers = []
-    for i in range(0, 10):
+    iters = 50
+    for i in range(0, iters):
         seed = random.randrange(10, 500, 1)
         print("Creating {} random adventures: ".format(numAdv))
         advList = Adventure.createAdvList(numAdv, seed)
@@ -33,10 +34,12 @@ if __name__ == '__main__':
         print("\n Creating booker: ")
         booker = Booker(agentList, advList)
         upperBound = booker.upperBound
+        greedyBound = booker.greedyBound
+        print("Upper Bound for this game is: {} gold".format(upperBound))
+        print("Greedy Bound for this game is: {} gold".format(greedyBound))
         closedAdventures = booker.completedAdventures
         openAdventures = booker.adventures
-        booker.run(10, True)
-        print("Upper Bound for this game is: {} gold".format(upperBound))
+        booker.run(20, True)
         print(booker.reward)
         print(sum(booker.reward))
         print('Closed Adventures {}'.format(closedAdventures))
@@ -44,8 +47,15 @@ if __name__ == '__main__':
         for agent in booker.agents:
             print("{}: Income: {} Costs: {} Total: ".format(agent, agent.rewards, agent.finalCosts))
         bookers.append((booker, seed))
+    upperRatio = 0
+    greedyRatio = 0
     for b, s in bookers:
-        print("Seed: {}, UpperBound: {}, Completed {}, Ratio {}".format(s, b.upperBound, sum(b.reward), sum(b.reward)/b.upperBound))
+        print("Seed: {}, UpperBound: {}, GreedyBound {}, Completed {}, Upper Ratio {}, Greedy Ratio".format(s, b.upperBound, b.greedyBound, sum(b.reward), 
+            sum(b.reward)/b.upperBound, sum(b.reward)/b.greedyBound))
+        upperRatio += sum(b.reward)/b.upperBound
+        greedyRatio += sum(b.reward)/b.greedyBound
+    print ('Average Upper Ratio = {}', upperRatio/iters)
+    print ('Average Greedy Ratio = {}', greedyRatio/iters)
 
 
 
