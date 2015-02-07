767ef8ac34a8e45237d659fa867a1c92cb43f8a3
mitch <mitchfizz@gmail.com>
2015-02-07 13:00:58 +0100

    added plotting

---
+++ MultiAgentAdventures/Plot.py
@@ -0,0 +1,74 @@
+import random
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+from itertools import accumulate
+import tkinter as Tk
+import tkinter.messagebox
+
+class PlotClassifier(Tk.Tk):
+    def __init__(self, plot_generator, arguments, classes, classification_callback, *args, **kwargs):
+        Tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Plotting of %i games" % len(arguments))
+        self._plot_generator = plot_generator
+        self._arguments = arguments
+        self._classes = [str(x) for x in classes]
+        self._classification_callback = classification_callback
+        self._setup_gui()
+
+    def _setup_gui(self):
+        #self.columnconfigure(0, minsize=100, weight=2)
+        #self.columnconfigure(1, minsize=500, weight=8)
+        f = Figure()
+        self._ax = f.add_subplot(111)
+        buttons_frame = Tk.Frame(self)
+        buttons_frame.pack(side=Tk.TOP, fill=Tk.BOTH, expand=True)
+        buttons_class = []
+        for i, cls in enumerate(self._classes):
+            buttons_class.append(Tk.Button(master=buttons_frame, text=cls, 
+                                           command=lambda x=i: self.button_classification_callback(self._current_args, x)))
+            buttons_class[-1].pack(side=Tk.LEFT)
+        button_quit = Tk.Button(master=buttons_frame, text='Quit', command=self.destroy)
+        button_quit.pack(side=Tk.RIGHT) #.grid(row=0,column=0)
+
+        self._canvas = FigureCanvasTkAgg(f, master=self)
+        self._canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1) #.grid(row=0, column=1, rowspan=3) #
+        self._canvas.show()
+
+        toolbar = NavigationToolbar2TkAgg( self._canvas, self )
+        toolbar.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1) #.grid(row=3, column=1) #
+        toolbar.update()
+
+    def button_classification_callback(self, args, class_idx):
+        self._classification_callback(args, self._classes[class_idx])
+        self.classify_next_plot()
+
+    def classify_next_plot(self):
+        try:
+            self._current_args = self._arguments.pop(0)
+            self._ax.cla()
+            self._plot_generator(self._ax, self._current_args)
+            self._canvas.draw()
+        except IndexError:
+            tkinter.messagebox.showinfo("Complete!", "All plots were classified")
+            self.destroy()        
+
+def create_plot(ax, args):
+    ax.set_ylabel("Gold")
+    ax.set_xlabel("Iterations")
+    ax.plot(args[0], label = "Algorithm")
+    ax.plot(args[1], label = "UpperBound")
+    ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
+
+def announce_classification(arguments, class_):
+    print(arguments, class_)
+
+def plot(bookers):
+    classes = ["Seed %i"%s for b,s in bookers]
+    print([(list(accumulate(b.reward)), [b.upperBound for i in range(0, len(b.reward))]) for b,s in bookers])
+    arguments_for_plot = [(list(accumulate(b.reward)), [b.upperBound for i in range(0, len(b.reward))]) for b,s in bookers]
+    root = PlotClassifier(create_plot, arguments_for_plot, classes, classification_callback=announce_classification)
+    root.after(50, root.classify_next_plot)
+    root.mainloop()
--- MultiAgentAdventures/Starter.py
+++ MultiAgentAdventures/Starter.py
@@ -3,7 +3,7 @@ Every class and parameter used to get different game outcomes should be set here
 game execution/performance should be contained here as much as possible, till we need dedicated modules for that.
 """
 
-import Agent, Skill, Adventure, random
+import Agent, Skill, Adventure, random, Plot
 from Booker import Booker
 
 """ number of random generated Adventures"""
@@ -13,9 +13,8 @@ numAgents = 15
 
 
 if __name__ == '__main__':
-
     bookers = []
-    for i in range(0, 10):
+    for i in range(0, 1):
         seed = random.randrange(10, 500, 1)
         print("Creating {} random adventures: ".format(numAdv))
         advList = Adventure.createAdvList(numAdv, seed)
@@ -35,7 +34,7 @@ if __name__ == '__main__':
         upperBound = booker.upperBound
         closedAdventures = booker.completedAdventures
         openAdventures = booker.adventures
-        booker.run(10, True)
+        booker.run(10)
         print("Upper Bound for this game is: {} gold".format(upperBound))
         print(booker.reward)
         print(sum(booker.reward))
@@ -44,8 +43,11 @@ if __name__ == '__main__':
         for agent in booker.agents:
             print("{}: Income: {} Costs: {} Total: ".format(agent, agent.rewards, agent.finalCosts))
         bookers.append((booker, seed))
+
     for b, s in bookers:
-        print("Seed: {}, UpperBound: {}, Completed {}".format(s, b.upperBound, sum(b.reward)))
+        print("Seed: {}, UpperBound: {}, Completed {}, Percentage:= {}".format(s, b.upperBound, sum(b.reward), sum(b.reward)/b.upperBound))
+    interestingSeeds = [(b,s) for b,s in bookers if sum(b.reward)/b.upperBound > 0.8 or sum(b.reward)/b.upperBound < 0.4]
+    for b, s in interestingSeeds:
+        print("Seed: {}, UpperBound: {}, Completed {}, Percentage:= {}".format(s, b.upperBound, sum(b.reward), sum(b.reward)/b.upperBound))
 
-
-
+    Plot.plot(bookers)
